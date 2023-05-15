dbe510772494b8687b3eea1c443b064560e8d045
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-05-15 10:57:09 +0400

    changed bin/brain-games.js , package-lock.json , package.json

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!")
+
+import {userName} from '/root/Project-1/frontend-project-44/bin/src/cli.js' 
+
+console.log("Welcome to the Brain Games!" + userName)
