5d37775a2ceb0f7b93e903bee3935b62b85eefd2
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-05-19 00:45:44 +0400

    changed bin/brain-even.js bin/src/cli.js bin/src/index.js

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
  export const userName = () => {
     const name = readlineSync.question('May I have your name? ');
     return name
-    //  console.log('Hello' + ', ' + name + '!')
 } 
 
 
--- bin/src/index.js
+++ bin/src/index.js
@@ -1,9 +1,49 @@
 import readlineSync from 'readline-sync'
 
 export const QuestionEvenOrOddNumber = () => {
-    const number = Math.ceil(Math.random() * 100)
-    const question = readlineSync.question('Question: ' + number + ' ');
-    return question 
+const number = Math.ceil(Math.random() * 100)
+
+let answer = ''
+
+if (number % 2 === 0 && answer === 'yes') {
+  const game = readlineSync.question('Question: ' + number + ' ') 
+  answer = 'good'
+  console.log(game)
+  console.log('Your answer: ' + number)
+  return answer 
+} else {
+  const game = readlineSync.question('Question: ' + number + ' ')
+  answer = 'bad'
+  console.log(game)
+  console.log('Your answer: ' + number)
+  return answer 
+}
 }
 
-// возваращается вывод return 
+
+// export const QuestionEvenOrOddNumber = () => {
+//   const number = Math.ceil(Math.random() * 100)
+//     if (readlineSync.keyInYN('Question: ' + number + ' ') && number % 2 === 0) {
+//   // 'Y' key was pressed.
+//     console.log('good');
+// } else {
+//     console.log('bad');
+//   }
+// }
+ // если число четное и мы нажали да или число нечетное и мы нажали нет,то good 
+// если число нечетное и мы нажали да или число четное и мы нажали нет,то bad
+
+// подходит ли нам вообще именно этот метод?
+
+// export const game = () => {
+//   const rounds = 3
+//   const number = Math.ceil(Math.random() * 100)
+//   for (const round of rounds) {
+//   const answer = {goodAnswer, badAnswer}
+//   if () {
+
+//   }
+// }
+// }
+
+
