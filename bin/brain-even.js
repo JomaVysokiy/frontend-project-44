de6d3f3faf90f3dfb361cf5335e43df111603bbf
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-08-18 13:36:59 +0400

    modified:   bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,64 +6,61 @@ const game = () => {
   console.log(`Hello, ${name}!`)
   console.log('Answer "yes" if the number is even, otherwise answer "no".')
        
-  const randomNumber1 = Math.ceil(Math.random() * 100)
-  const randomNumber2 = Math.ceil(Math.random() * 100)
-  const randomNumber3 = Math.ceil(Math.random() * 100)
-  const numbers = [randomNumber1,randomNumber2,randomNumber3]
-  
-  let rightAnswers = 0
-  
   for (let i = 0 ; i < 3 ; i++) {
-    // const number = Math.ceil(Math.random() * 100)
-    console.log('Question: ' + numbers[i])
+    let number = Math.ceil(Math.random() * 100)
+    
+    console.log('Question: ' + number)
     const answer = readlineSync.question('Your answer: ')
 
     const numberIsEven = (number) => number % 2 === 0  
     const isCorrectAnswer = (answer) => {
-        if (numberIsEven(numbers[i]) && answer === 'yes') {         
-          return true
+        if (numberIsEven(number) && answer === 'yes') {  
+          return true 
         }
-        if (!numberIsEven(numbers[i]) && answer === 'no') {
+        if (!numberIsEven(number) && answer === 'no') {         
           return true
         }
       }
-
-    // const isCorrectAnswer2 = (answer) => {     
-    //   if (numberIsEven(numbers[i]) && answer === 'yes') {
-    //       answer = 'yes'
-    //     } else if (!numberIsEven(numbers[i]) && answer === 'no') {
-    //       answer = 'no'
-    //     }
-    // }
 
     if (isCorrectAnswer(answer)) {
       console.log('Correct!')
-      rightAnswers += 1
-      if (rightAnswers === 3) {
-        console.log(`Congratulations, ${name}!`)
+    } else {     
+      const isNotCorrectAnswer = (answer) => {
+        if (answer !== 'yes' || answer !== 'no') {
+          return true
+        }
       }
-    } else {                 
-      if (numberIsEven(numbers[i]) && answer === 'yes') {
+
+      // просто пропускает или не видит коректный ответ
+    if (isNotCorrectAnswer(answer)) {
+      if (numberIsEven(answer) && isCorrectAnswer(answer)) {
         const correctAnswer = 'yes'
         console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-        break
-      } else if (!numberIsEven(numbers[i]) && answer === 'no') {
+        console.log(`Let's try again, ${name}!`)
+        return
+      } else if (!numberIsEven(number) && answer === 'no') {
         const correctAnswer = 'no'
         console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-        break
+        console.log(`Let's try again, ${name}!`)
+        return
       }
-    
+
+    }
+      
+    // я пытался упростить эти условия но не могу вывести именно правильный ответ
       if (answer === 'yes' ) {
         console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`)
         console.log(`Let's try again, ${name}!`)
-        break
+        return
       } else if (answer === 'no') {
         console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.`)
         console.log(`Let's try again, ${name}!`)
-        break
+        return
       }      
-    }
+      break
+    } 
   }
+  console.log(`Congratulations, ${name}!`)
 }
 
 game()
