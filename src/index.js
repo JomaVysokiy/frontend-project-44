ba66b8bdec34f0e0b644d450ab8cbd625df2abcf
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-08-22 10:15:03 +0400

    changed index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import readlineSync from 'readline-sync';
 const game = (descriptionOfExercise,game) => {
     // приветствие
     console.log('Welcome to the Brain Games!')
-    const name = readlineSync.question('May I have your name? ')
-    console.log(`Hello, ${name}!`)
+    const userName = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${userName}!`)
     // описание задания 
     console.log(descriptionOfExercise)
     // цикл
@@ -16,17 +16,17 @@ const game = (descriptionOfExercise,game) => {
         // как будет(или не будет) работать без них?
         const [correctAnswer, question] = game()
         console.log('Question: ' + question)
-        const answer = readlineSync.question('Your answer: ')
+        const yourAnswer = readlineSync.question('Your answer: ')
     
         if (answer !== correctAnswer) {
-          console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-          console.log(`Let's try again, ${name}!`)
+          console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+          console.log(`Let's try again, ${userName}!`)
           return
         } else {
           console.log('Correct!')         
         }    
     }
-    console.log(`Congratulations, ${name}!`)
+    console.log(`Congratulations, ${userName}!`)
 }
 
 export default game
