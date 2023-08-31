4015035dbb22eba1210baf8ef6c8a3c504a37aee
JomaVysokiy <114975316+JomaVysokiy@users.noreply.github.com>
2023-08-31 12:00:00 +0400

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,40 +1,5 @@
 #!/usr/bin/env node
 import game from '../index.js'
-// const game = () => {
-//   console.log('Welcome to the Brain Games!');
-//   const userName = readlineSync.question('May I have your name? ');
-//   console.log(`Hello, ${userName}!`);
-
-//   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-//   for (let i = 0; i < 3; i += 1) {
-    // const num = Math.ceil(Math.random() * 100)
-    // const question = `${num}`
-      
-    // const isPrime = (num) => {
-    //   if (num < 2) {
-    //     return false
-    //   }
-    
-    //   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    //     return num % i === 0 ? false : true
-    //   }
-    // }
-    // let correctAnswer = ''
-    
-    // isPrime(num) === true ? correctAnswer = 'yes' : correctAnswer = 'no'
-    // console.log(`Question: ${question}`)
-    // const yourAnswer = readlineSync.question('Your answer: ')
-//     if (yourAnswer === correctAnswer) {
-//       console.log('Correct!')
-//     } else {
-//       console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-//       console.log(`Let's try again, ${userName}!`)
-//       return;
-//     }
-//   }
-//   console.log(`Congratulations, ${userName}!`)
-// }
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 const isPrime = (num) => {
@@ -50,7 +15,6 @@ const isPrime = (num) => {
 
   return true
 };
-
 
 const primeGame = () => {
   const runGame = () => {
