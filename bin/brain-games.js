4dc57da899ea187a3354c7da1fc0de66908c76f9
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-09-02 06:40:20 +0400

    modified:   brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import greeting from '/root/projects/frontend-project-44/src/cli.js'
+// import greeting from '/root/projects/frontend-project-44/src/cli.js'
+import greeting from '../src/cli.js'
 
 greeting()
 
