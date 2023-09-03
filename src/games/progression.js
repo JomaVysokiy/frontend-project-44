c39d06ee04dd38c67efdba6c894f72335a2fa31a
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-09-03 13:21:12 +0400

    changed src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,20 +5,23 @@ const description = 'What number is missing in the progression?';
 
 const progressionGame = () => {
     const runGame = () => {
+        // первые два числа прогрессии
         const num1 = Math.ceil(Math.random() * 100);
         const num2 = Math.ceil(Math.random() * 100);
-
+        // создание массива с первым числом
         const progression = [num1];
-
-        for (let i = 0; i <= 8; i++) {
+        // цикл
+        for (let i = 0; i <= 8; i += 1) {
             progression.push(progression[i] + num2);
         };
-      
+        // создание индекса 
         const randomIndex = Math.ceil(Math.random() * 9);
+        // правильный ответ
         const correctAnswer = `${progression[randomIndex]}`;
+        // замена правильного ответа на '..'
         progression[randomIndex] = '..';
+        // вопрос и возврат
         const question = `${progression.join(' ')}`;
-        
         return [question, correctAnswer];
     };
     game(description, runGame);
