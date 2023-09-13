8d3389d0fe77b25a86139f8acb6eac21935b0974
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-09-13 07:01:56 +0400

    cahnged index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import readlineSync from 'readline-sync';
 
 const game = (description, generateRound) => {
-  
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  
+
   console.log(description);
   const rounds = 3;
 
   for (let i = 0; i < rounds; i += 1) {
-    
     const [question, correctAnswer] = generateRound();
     console.log(`Question: ${question}`);
-    
     const answer = readlineSync.question('Your answer: ');
 
-    
     if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
