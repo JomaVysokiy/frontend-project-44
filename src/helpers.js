2f45f9d7c5dbc99c170d92ef7fe3604243990da7
JomaVysokiy <114975316+JomaVysokiy@users.noreply.github.com>
2023-09-19 09:50:30 +0400

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,4 @@
 export default (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  const result = Math.round(Math.random() * (max - min) + min);
+  return result;
 };
